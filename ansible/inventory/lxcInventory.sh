32925da0b1b1cf9591886288adb1d551103df179
Maxime BOSSARD <mxbossard@gmail.com>
2018-02-02 21:40:12 +0100

    Use ssh to speed up lxc inventory.

--- ansible/inventory/lxcInventory.sh
+++ ansible/inventory/lxcInventory.sh
@@ -1,7 +1,25 @@
 #! /bin/sh
 
-inventoryPath="$(dirname $0)"
-lxcInventory="/home/vagrant/ansible/inventory/vagrant.test"
+inventoryScript="/home/lxc/lxcInventory.py"
+lxcUser="lxc"
+args="$@"
 
-ansible lxc_hosts -o -i $lxcInventory --become --become-user lxc -a "/bin/sh -c '/home/lxc/lxcInventory.py $*'" | grep -o -e "{.*}"
+# Inventory lxc hosts with ansible
+ansibleInventory() {
+	lxcInventory="/home/vagrant/ansible/inventory/vagrant.test"
+	lxcHostsGroup="lxc_hosts"
+
+	ansible $lxcHostsGroup -o -i $lxcInventory --become --become-user $lxcUser -a "/bin/sh -c '$inventoryScript $args'" | grep -o -e '{.*}'
+}
+
+# Inventory lxc hosts with ssh
+sshInventory() {
+	sshKeyFile="~/.vagrantSsh/insecure_private_key"
+	lxcHost="amipo1"
+	
+	ssh -i $sshKeyFile $lxcHost "sudo -u $lxcUser -i /bin/sh -c '$inventoryScript $args'"
+}
+
+sshInventory
+#ansibleInventory
 
